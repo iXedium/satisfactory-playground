d63f19f60d5e34464fc0ac5d8f0f2d3c42ff7f80
Alireza Ershadi <alireza.ershadi@gmail.com>
2026-08-26 22:47:14 +0100

    feature: add new power recipes for Rocket Fuel and Ionized Fuel

--- src/data/dexieInit.ts
+++ src/data/dexieInit.ts
@@ -2,7 +2,7 @@ import { db } from "./dexieDB";
 import { Item } from "../types";
 
 const DB_VERSION_KEY = 'satisfactory-db-version';
-const CURRENT_VERSION = 17;
+const CURRENT_VERSION = 20;
 
 export async function populateDexie() {
     try {
